4e7a262e1a79f25bdf7ac90ab78b790cf14b02b8
edmond120 <edmondwong888@msn.com>
2017-10-05 08:38:15 -0400

    done

--- app.py
+++ app.py
@@ -4,15 +4,10 @@ import os
 form_site = Flask(__name__)
 form_site.secret_key = os.urandom(16)
 
-@form_site.route("/", methods=['POST', 'GET'])
-def root():
-    print request.form
-    return render_template('temp.html')
-
 @form_site.route("/welcome", methods=['POST', 'GET'])
 def welcome():
     if(len(session) > 0):
-        username = session["username"]
+        username = session["username"];
         password = session["password"]
     else:
         username = request.form["username"]
@@ -26,6 +21,16 @@ def welcome():
     session["username"] = request.form["username"]
     session["password"] = request.form["password"]
     return render_template('access.html', name=request.form["username"])
+
+@form_site.route("/", methods=['POST', 'GET'])
+def root():
+    if(len(session) > 0):
+        print "1"
+        if(session.get("username")=="bob" and session.get("password")=="123"):
+            return render_template('access.html',name=session["username"])
+    
+    print request.form
+    return render_template('temp.html')
 
 if __name__ == '__main__':
     form_site.debug = True
